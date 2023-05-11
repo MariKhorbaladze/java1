5a2b838f775cb09968ccec61a6edf74339b9f815
MariKhorbaladze <129887035+MariKhorbaladze@users.noreply.github.com>
2023-05-12 01:57:20 +0400

    jv1

---
+++ src/script.js
@@ -0,0 +1,25 @@
+console.log("Enter Your price")
+alert("Enter Your price")
+document.write("<h1>hi</h1>")
+
+var salary = prompt("Enter Your ფrice")
+
+
+if  (salary > 3500) { 
+    alert ("kai xelpasia");
+    
+}
+
+
+else if (salary > 1500 && salary < 2500);  {
+    alert ("SASHUALO XELPASi");
+} 
+if (salary < 2500) {
+    alert ("gaagde saxlidan");
+}
+
+
+
+
+
+
